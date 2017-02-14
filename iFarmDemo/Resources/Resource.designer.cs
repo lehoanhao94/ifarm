7b8f7444ef3c6dd220a2659b1e1abd0666a2b079
haolh <haolh>
2017-02-14 17:05:15 +0700

    jhkjhkj

--- iFarmDemo/Resources/Resource.designer.cs
+++ iFarmDemo/Resources/Resource.designer.cs
@@ -2617,70 +2617,73 @@ namespace HelloToolbar
 			public const int detail = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int main = 2130903069;
+			public const int item_Category = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int notification_action = 2130903070;
+			public const int main = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int notification_action_tombstone = 2130903071;
+			public const int notification_action = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int notification_media_action = 2130903072;
+			public const int notification_action_tombstone = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int notification_media_cancel_action = 2130903073;
+			public const int notification_media_action = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int notification_template_big_media = 2130903074;
+			public const int notification_media_cancel_action = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int notification_template_big_media_custom = 2130903075;
+			public const int notification_template_big_media = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int notification_template_big_media_narrow = 2130903076;
+			public const int notification_template_big_media_custom = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int notification_template_big_media_narrow_custom = 2130903077;
+			public const int notification_template_big_media_narrow = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int notification_template_custom_big = 2130903078;
+			public const int notification_template_big_media_narrow_custom = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_template_icon_group = 2130903079;
+			public const int notification_template_custom_big = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_template_lines_media = 2130903080;
+			public const int notification_template_icon_group = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_media = 2130903081;
+			public const int notification_template_lines_media = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_media_custom = 2130903082;
+			public const int notification_template_media = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_template_part_chronometer = 2130903083;
+			public const int notification_template_media_custom = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_template_part_time = 2130903084;
+			public const int notification_template_part_chronometer = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int select_dialog_item_material = 2130903085;
+			public const int notification_template_part_time = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int select_dialog_multichoice_material = 2130903086;
+			public const int select_dialog_item_material = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int select_dialog_singlechoice_material = 2130903087;
+			public const int select_dialog_multichoice_material = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int support_simple_spinner_dropdown_item = 2130903088;
+			public const int select_dialog_singlechoice_material = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int toolbar = 2130903089;
+			public const int support_simple_spinner_dropdown_item = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int web = 2130903090;
+			public const int toolbar = 2130903090;
+			
+			// aapt resource value: 0x7f030033
+			public const int web = 2130903091;
 			
 			static Layout()
 			{
--- iFarmDemo/Sources/CategoryActivity.cs
+++ iFarmDemo/Sources/CategoryActivity.cs
@@ -11,6 +11,8 @@ using Android.Views;
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
 using Com.Lilarcor.Cheeseknife;
+using iFarm.Entities;
+using Android.Widget;
 
 namespace HelloToolbar.Sources
 {
@@ -26,8 +28,8 @@ namespace HelloToolbar.Sources
         RecyclerView rvCategoryList;
 
         RecyclerView.LayoutManager mLayoutManager;
-        //PhotoAlbumAdapter mAdapter;
-        //PhotoAlbum mPhotoAlbum;
+        CategoryAdapter mCategoryAdapter;
+        List<Category> mLstCategory;
         #endregion
 
         #region CONSTRUCTORS
@@ -43,7 +45,16 @@ namespace HelloToolbar.Sources
             Cheeseknife.Inject(this);
 
             setupToolbar();
-        }  
+
+            prepareDemoData();
+
+            mCategoryAdapter = new CategoryAdapter(mLstCategory);
+            rvCategoryList.SetAdapter(mCategoryAdapter);
+
+            mLayoutManager = new LinearLayoutManager(this);
+            rvCategoryList.SetLayoutManager(mLayoutManager);
+        }
+
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             if (item.ItemId == Android.Resource.Id.Home)
@@ -60,6 +71,27 @@ namespace HelloToolbar.Sources
 
         #region OTHERS
 
+        private void prepareDemoData()
+        {
+            mLstCategory = new List<Category>();
+
+            List<Disease> _lstDisease = new List<Disease>();
+            _lstDisease.Add(new Disease(1, "Thối gốc 1"));
+            _lstDisease.Add(new Disease(2, "Thối gốc 2"));        
+            _lstDisease.Add(new Disease(3, "Thối gốc 3"));
+            _lstDisease.Add(new Disease(4, "Thối gốc 4"));
+            _lstDisease.Add(new Disease(5, "Thối gốc 5"));
+            _lstDisease.Add(new Disease(6, "Thối gốc 6"));
+            _lstDisease.Add(new Disease(7, "Thối gốc 7"));
+
+            mLstCategory.Add(new Category(1, "Cây cao su 1", "http://www.caosubinhduong.com.vn/images/news/cao-su-nuoc.jpg", _lstDisease));
+            mLstCategory.Add(new Category(2, "Cây cao su 2", "http://www.caosubinhduong.com.vn/images/news/cao-su-nuoc.jpg", _lstDisease));
+            mLstCategory.Add(new Category(3, "Cây cao su 3", "http://www.caosubinhduong.com.vn/images/news/cao-su-nuoc.jpg", _lstDisease));
+            mLstCategory.Add(new Category(4, "Cây cao su 4", "http://www.caosubinhduong.com.vn/images/news/cao-su-nuoc.jpg", _lstDisease));
+            mLstCategory.Add(new Category(5, "Cây cao su 5", "http://www.caosubinhduong.com.vn/images/news/cao-su-nuoc.jpg", _lstDisease));
+
+        }
+
         private void setupToolbar()
         {
             SetSupportActionBar(toolbar);
@@ -71,5 +103,52 @@ namespace HelloToolbar.Sources
         }
 
         #endregion
+    }
+
+    public class CategoryAdapter : RecyclerView.Adapter
+    {
+        public List<Category> mLstCategory;
+
+        public CategoryAdapter(List<Category> lstCategory)
+        {
+            mLstCategory = lstCategory;
+        }
+
+        public override int ItemCount
+        {
+            get
+            {
+                return mLstCategory.Count;
+            }
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            // Inflate the CardView for the photo:
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.PhotoCardView, parent, false);
+
+            // Create a ViewHolder to hold view references inside the CardView:
+            CategoryItemViewHolder vh = new CategoryItemViewHolder(itemView);
+            return vh;
+        }
+    }
+
+    public class CategoryItemViewHolder : RecyclerView.ViewHolder
+    {
+        public ImageView Image { get; private set; }
+        public TextView Caption { get; private set; }
+
+        public CategoryItemViewHolder(View itemView) : base(itemView)
+        {
+            // Locate and cache view references:
+            Image = itemView.FindViewById<ImageView>(Resource.Id.imageView);
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+        }
     }
 }
