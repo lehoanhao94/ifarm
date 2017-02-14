9a1a82ad6ed18de8e31dad9987e279b9102ab049
kupinsit <mr.cnguyen94@gmail.com>
2017-02-14 16:50:20 +0700

    vbvb

---
+++ iFarmDemo/obj/Debug/android/src/md52b4032fbd64f7468a37c15f28a4965f1/BaseActivity.java
@@ -0,0 +1,56 @@
+package md52b4032fbd64f7468a37c15f28a4965f1;
+
+
+public class BaseActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onKeyDown:(ILandroid/view/KeyEvent;)Z:GetOnKeyDown_ILandroid_view_KeyEvent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("HelloToolbar.Sources.BaseActivity, HelloToolbar, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", BaseActivity.class, __md_methods);
+	}
+
+
+	public BaseActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == BaseActivity.class)
+			mono.android.TypeManager.Activate ("HelloToolbar.Sources.BaseActivity, HelloToolbar, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onKeyDown (int p0, android.view.KeyEvent p1)
+	{
+		return n_onKeyDown (p0, p1);
+	}
+
+	private native boolean n_onKeyDown (int p0, android.view.KeyEvent p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
